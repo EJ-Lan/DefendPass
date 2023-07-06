88a78b9a3125724e9f6e49a254b79c7fe3812d09
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-06 10:58:28 -0600

    Finish functionalities and add main method

--- password_checker.py
+++ password_checker.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -9,19 +10,30 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try againgit')
     return res
 
+
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(first5_char, tail)
-    print(response)
     return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} time... you should probably change your password!')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return
+
+
+main(sys.argv[1:])
