440177c84d3106d273a5a568bca94b40fd286302
EJ-Lan <ervinjon_lanada@outlook.com>
2023-07-06 10:37:44 -0600

    Set up functions for program

--- password_checker.py
+++ password_checker.py
@@ -3,3 +3,10 @@ import requests
 url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
 res = requests.get(url)
 print(res)
+
+def request_api_data(query_char):
+    pass
+
+def pwned_api_check(password):
+    #Check password if it exists in API response
+    pass
